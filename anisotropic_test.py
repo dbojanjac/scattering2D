4c08119753ca821acc36e4c3f0f353591b05f6b3
Darko Janekovic <darko.janekovic@gmail.com>
2020-10-05 09:37:39 +0200

    update demos

--- anisotropic_test.py
+++ anisotropic_test.py
@@ -2,14 +2,9 @@ import numpy as np
 
 from scattering import *
 
-mesh_file = "mesh/hexa.msh"
-epsilon = ((5.41474, 0), (0, 5.71539))
-
-permittivity_dict = {
-        1: epsilon,
-        2: epsilon,
-        3: ((1, 0), (0, 1))
-}
+mesh_file = "mesh/homo.msh"
+epsilon = [[5.47781441, 0. ], [0., 5.78054277]]
+permittivity_dict = {1: epsilon, 2: epsilon, 3: np.identity(2)}
 s = np.array([1, 2])
 p = np.array([-2, 1])
 k0L = np.pi
@@ -19,5 +14,5 @@ pw = PlaneWave(s, p)
 
 E = problem.solve(pw)
 
-phi, FF = problem.get_far_field(E, 40)
-plot_far_field(phi, FF, "anisotropic")
+#phi, FF = problem.get_far_field(E, 40)
+#plot_far_field(phi, FF, "anisotropic")
--- isotropic_test.py
+++ isotropic_test.py
@@ -2,7 +2,7 @@ import numpy as np
 from scattering import *
 
 mesh_file = "mesh/hexa.msh"
-permittivity_dict = {1: 1, 2: 11.7, 3: 1}
+permittivity_dict = {1: 1, 2: 11.8, 3: 1}
 s = np.array([1, 2])
 p = np.array([-2, 1])
 k0L = np.pi
@@ -12,5 +12,5 @@ pw = PlaneWave(s, p)
 
 E = problem.solve(pw)
 
-phi, FF = problem.get_far_field(E, 40)
-plot_far_field(phi, FF, "isotropic")
+#phi, FF = problem.get_far_field(E, 40)
+#plot_far_field(phi, FF, "isotropic")
