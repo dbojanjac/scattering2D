654c974648b20b330abb139ee25fe8b4dc17e836
Darko Janekovic <darko.janekovic@gmail.com>
2020-11-26 10:37:39 +0100

    scattering: add documentation
    Signed-off-by: Darko Janekovic <darko.janekovic@gmail.com>

--- scattering/plane_wave.py
+++ scattering/plane_wave.py
@@ -1,13 +1,26 @@
-import firedrake as fd
 import numpy as np
+from abc import ABC, abstractmethod
+
+from firedrake import VectorFunctionSpace, SpatialCoordinate, interpolate, dot, exp, as_vector
+
+from .scattering import ExcitationBase
 
 def orthogonal(s, p):
+    """Return true if np.arrays that represent vectors are orthogonal"""
     return np.isclose(np.dot(s, p), 0)
 
 def normalize(a):
+    """Normalize vector a represented as np.array"""
     return a / np.linalg.norm(a)
 
-class PlaneWave():
+class PlaneWave(ExcitationBase):
+    """
+    Plane wave excitation
+
+    We define plane wave as:
+        E_i(x) = \hat{p} * exp(i k0L * \hat{s} \cdot x)
+    """
+
     def __init__(self, s, p):
         # If vectors aren't orthogonal, make them orthogonal by ignoring
         # electric field component in the direction of propagation
@@ -20,14 +33,13 @@ class PlaneWave():
         s = normalize(s)
         p = normalize(p)
 
-        self.s = fd.as_vector(s)
-        self.p = fd.as_vector(p)
+        self.s = as_vector(s)
+        self.p = as_vector(p)
 
-    #TODO: this should be made abtract and other excitements should implement
-    def interpolate(self, mesh, k0L):
-        V = fd.VectorFunctionSpace(mesh, "CG", 1)
-        x = fd.SpatialCoordinate(mesh)
+    def interpolate(self, mesh, *args):
+        """Interpolate plane wave on the mesh"""
+        (k0L,) = args
+        V = VectorFunctionSpace(mesh, "CG", 1)
+        x = SpatialCoordinate(mesh)
 
-        pw = fd.interpolate(self.p * fd.exp(1j * k0L * fd.dot(self.s, x)), V)
-
-        return pw
+        return interpolate(self.p * exp(1j * k0L * dot(self.s, x)), V)
--- scattering/scattering.py
+++ scattering/scattering.py
@@ -2,20 +2,66 @@ import firedrake as fd
 import numpy as np
 from abc import ABC, abstractmethod
 
+class ExcitationBase(ABC):
+    """Abstract Excitation object"""
+
+    @abstractmethod
+    def interpolate(self, mesh, *args):
+        """Interpolate Excitation on the mesh"""
+        pass
+
+
 class Scattering(ABC):
-    def __init__(self, mesh, k0L, output_dir="output"):
+    """
+    Base class for all Scattering problems.
+    Scattering object is wrapper above problem description and domain where problem
+    is defined.
+    """
+
+    def __init__(self, mesh, k0L):
+        """
+        Initialize Scattering object
+
+        Parameters:
+        -----------
+        mesh: either path to the file with extension of some supported mesh format[1] or
+              already created firedrake mesh. One should use firedrake mesh in order to
+              do a errornorm between two solutions on the same mesh.
+        k0L:  wave number
+
+        [1] - https://www.firedrakeproject.org/firedrake.html#firedrake.mesh.Mesh
+        """
         if isinstance(mesh, str):
             self.mesh = fd.Mesh(mesh)
         elif isinstance(mesh, fd.firedrake.mesh.MeshGeometry):
             self.mesh = mesh
         else:
             raise ValueError("Mesh object unknown")
-        self.output_dir = output_dir
         self.k0L = k0L
 
 
     def solve(self, incident, method="lu"):
-        Ei = incident.interpolate(self.mesh, self.k0L)
+        """
+        Solve Maxwell Scattering problem using incident excitation
+
+        We are solving:
+
+        curl(curl(E)) - k^2 epsilon_r * E = 0
+        E = E_i + E_s where E_i is incident wave
+
+        As a boundary conditions we are using first order Silver-Muller BC.
+
+        Function will compute E_s and return E = E_i + E_s.
+
+        Parameters:
+        -----------
+        incident: object that should inherit from ExcitationBase
+        method:   method for solving Ax=b system. Currently we default to MUMPS.
+        """
+        if isinstance(incident, ExcitationBase):
+            Ei = incident.interpolate(self.mesh, self.k0L)
+        else:
+            raise ValueError("incident object unknown")
 
         V = fd.FunctionSpace(self.mesh, 'N1curl', 1)
         Es = fd.Function(V)
@@ -30,10 +76,12 @@ class Scattering(ABC):
 
         a = (fd.inner(fd.curl(u), fd.curl(v)) * fd.dx
             - k**2 * fd.inner(epsilon * u, v) * fd.dx)
-        if (self.mesh.topological_dimension() == 3):
+        if self.mesh.topological_dimension() == 3:
                 a -= ik * fd.inner((fd.cross(n, fd.cross(n, u))), v) * fd.ds
         else:
-                a -= ik * ((fd.dot(u, n) * fd.inner(n, v)) - fd.inner(u, v)) * fd.ds
+                # for 2D case we have to write a \times b \times c as:
+                # (a \cdot c) \cdot b - (a \cdot b) \cdot c
+                a -= ik * ((fd.dot(n, u) * fd.inner(n, v)) - fd.inner(u, v)) * fd.ds
         L = - k**2 * fd.inner((self.II - epsilon) * Ei, v) * fd.dx
 
         solvers = {
@@ -55,6 +103,23 @@ class Scattering(ABC):
 
 
     def get_far_field(self, E, ff_num_samples, theta=np.pi/2):
+        """Compute Far field for the electric field
+
+        Compute far field components using Mischenkos: Electromagnetic scattering by Particles
+        and Particle Groups. Cambridge University Press.
+
+        Parameters:
+        -----------
+        E:              electromagnetic field for which we are computing far field.
+        ff_num_samples: number of far field components. In other word, how much values will
+                        we sample along the circle. Points on the circle are equidistant.
+        theta:          angle with respect to polar axis. pi/2 generates xOy plane.
+
+        Returns:
+        --------
+        phi:     np.array of ff_num_samples angles where we computed far field.
+        ff_data: np.array of ff_num_samples which represent far field at each angle phi.
+        """
         phi = np.linspace(0, 2 * np.pi, num = ff_num_samples, endpoint = False)
         ff_data = np.zeros(ff_num_samples)
 
